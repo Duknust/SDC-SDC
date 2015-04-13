a6c407edf09d8fd1a299a1740bfc75e4578bf0c9
Duarte Duarte <duartenuno1992@hotmail.com>
2015-04-13 16:32:26 +0100

    [Add] Recover methods

---
+++ lab2/ServerBank/src/communication/OpRecover.java
@@ -0,0 +1,9 @@
+//created by duknust
+//find in https://github.com/Duknust
+package communication;
+
+public class OpRecover extends Operation {
+
+    public OpRecover() {
+    }
+}
--- lab2/ServerBank/src/serverbank/Server.java
+++ lab2/ServerBank/src/serverbank/Server.java
@@ -3,9 +3,11 @@
 package serverbank;
 
 import bank.BankImpl;
+import com.sun.jmx.remote.internal.ArrayQueue;
 import communication.OpBalance;
 import communication.OpLeave;
 import communication.OpMove;
+import communication.OpRecover;
 import communication.Operation;
 import communication.ResBalance;
 import communication.ResLeave;
@@ -32,35 +34,46 @@ import net.sf.jgcs.jgroups.JGroupsService;
 
 public class Server implements MessageListener {
 
+    private static boolean startingFromZero = true;
+
     static BankImpl bank = null;
     static DataSession ds = null;
+    static ControlSession cs = null;
+    ArrayQueue<Message> queue = null;
+
     IpGroup gc = null;
     JGroupsGroup gg = null;
     Protocol p = null;
 
+    static int estado = 1;
+
+    //ip
     public Server(IpGroup gc, Protocol p) {
         try {
             this.gc = gc;
             this.p = p;
 
-            this.ds = p.openDataSession(gc);
-            this.ds.setMessageListener(this);
-            ControlSession cs = this.p.openControlSession(gc);
+            ds = p.openDataSession(gc);
+            ds.setMessageListener(this);
+            cs = this.p.openControlSession(gc);
             cs.join();
         } catch (GroupException ex) {
             Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
+    //group
     public Server(JGroupsGroup gg, Protocol p) {
         try {
             this.gg = gg;
             this.p = p;
 
-            this.ds = p.openDataSession(gg);
-            this.ds.setMessageListener(this);
-            ControlSession cs = this.p.openControlSession(gg);
+            ds = p.openDataSession(gg);
+            ds.setMessageListener(this);
+            cs = this.p.openControlSession(gg);
             cs.join();
+
+            this.queue = new ArrayQueue<>(100);
         } catch (GroupException ex) {
             Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -68,20 +81,39 @@ public class Server implements MessageListener {
 
     public static void main(String[] args) {
         try {
-            bank = new BankImpl();
-
-            System.out.println("[Server] Server started");
 
             //IpProtocolFactory pf = new IpProtocolFactory();
             JGroupsProtocolFactory gf = new JGroupsProtocolFactory();
-            JGroupsGroup gg = new JGroupsGroup("banco");
-
-            Protocol p = gf.createProtocol();
-            Server s = new Server(gg, p);
 
             //IpGroup gc = new IpGroup("225.1.2.3:12345");
             //Protocol p = pf.createProtocol();
             //Server s = new Server(gc, p);
+            JGroupsGroup gg = new JGroupsGroup("banco");
+            Protocol p = gf.createProtocol();
+            Server s = new Server(gg, p);
+
+            if (startingFromZero) {
+                bank = new BankImpl();
+            } else {
+                try {
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    ObjectOutputStream oos = new ObjectOutputStream(baos);
+
+                    Operation r = new OpRecover();
+                    oos.writeObject(r);
+                    byte[] data = baos.toByteArray();
+
+                    Message msg = ds.createMessage();
+                    msg.setPayload(data);
+
+                    //ds.multicast(msg, new IpService(), null);
+                    ds.multicast(msg, new JGroupsService(), null);
+
+                } catch (IOException ex) {
+                    Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            System.out.println("[Server] Server started");
             while (true) {
                 Thread.sleep(100000);
             }
@@ -92,52 +124,75 @@ public class Server implements MessageListener {
     }
 
     @Override
-    public Object onMessage(Message msg) {
+    public Object onMessage(Message msg
+    ) {
+
         System.out.println("HERE COMES A MESSAGE");
+        switch (estado) {
+            case 1:
+                if (msg.getSenderAddress().equals(cs.getLocalAddress())) {
+                    estado = 2;
+                }
+                break;
+            case 2:
+                try {
+                    ObjectInputStream oisHere = null;
+                    ByteArrayInputStream bais = new ByteArrayInputStream(msg.getPayload());
+                    oisHere = new ObjectInputStream(bais);
+                    Operation op = (Operation) oisHere.readObject();
+                    if (op instanceof OpRecover) {
+                        estado = 3;
+                        bank = new BankImpl();
+                        for (int i = 0; i < this.queue.size(); i++) {
+                            apply(this.queue.remove(i));
+                        }
+                    } else if (op instanceof OpMove) {
+                        this.queue.add(msg);
+                    }
+                } catch (IOException | ClassNotFoundException ex) {
+                    Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                break;
+            case 3:
+                apply(msg);
+                break;
+        }
+
+        return null;
+    }
+
+    private void apply(Message msg) {
         try {
             ObjectInputStream oisHere = null;
-            try {
-                ByteArrayInputStream bais = new ByteArrayInputStream(msg.getPayload());
-                oisHere = new ObjectInputStream(bais);
-                Operation op = (Operation) oisHere.readObject();
-                Response res = null;
-                if (op instanceof OpMove) {
-                    int value = ((OpMove) op).getAmount();
-                    boolean result = bank.move(value);
-                    res = new ResMove(result);
-                } else if (op instanceof OpBalance) {
-                    res = new ResBalance(bank.getBalance());
-                } else if (op instanceof OpLeave) {
-                    res = new ResLeave();
-                }
+            ByteArrayInputStream bais = new ByteArrayInputStream(msg.getPayload());
+            oisHere = new ObjectInputStream(bais);
+            Operation op = (Operation) oisHere.readObject();
+            Response res = null;
+            if (op instanceof OpMove) {
+                int value = ((OpMove) op).getAmount();
+                boolean result = bank.move(value);
+                res = new ResMove(result);
+            } else if (op instanceof OpBalance) {
+                res = new ResBalance(bank.getBalance());
+            } else if (op instanceof OpLeave) {
+                res = new ResLeave();
+            }
 
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                ObjectOutputStream oosHere = new ObjectOutputStream(baos);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ObjectOutputStream oosHere = new ObjectOutputStream(baos);
 
-                oosHere.writeObject(res);
-                byte[] data = baos.toByteArray();
-                Message toSend = ds.createMessage();
-                toSend.setPayload(data);
+            oosHere.writeObject(res);
+            byte[] data = baos.toByteArray();
+            Message toSend = ds.createMessage();
+            toSend.setPayload(data);
 
-                //ds.multicast(toSend, new IpService(), null, new PointToPoint(msg.getSenderAddress()));
-                ds.multicast(toSend, new JGroupsService(), null, new PointToPoint(msg.getSenderAddress()));
+            //ds.multicast(toSend, new IpService(), null, new PointToPoint(msg.getSenderAddress()));
+            ds.multicast(toSend, new JGroupsService(), null, new PointToPoint(msg.getSenderAddress()));
 
-            } catch (IOException | ClassNotFoundException ex) {
-                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
-            } /*finally {
-             try {
-             if (oisHere != null) {
-             oisHere.close();
-             }
-             } catch (IOException ex) {
-             Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
-             }
-             }*/
-
-        } catch (ClassCastException ex) {
-            System.out.println("discarding");
+        } catch (IOException | ClassNotFoundException ex) {
+            Logger.getLogger(Server.class
+                    .getName()).log(Level.SEVERE, null, ex);
         }
-        return null;
     }
 
 }
